37972a10d170e7187bb03b42e7cea43869d5dcc8
Jimmy Yeh <chienfeng0719@gmail.com>
2022-04-13 11:13:44 +0800

    Update 21

--- 21.py
+++ 21.py
@@ -5,24 +5,38 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not (list1 or list2):
-            return None
-        tmp = list()
-        while list1 and list2:
-            if not list1 or list1.val > list2.val:
-                tmp.append(list2.val)
-                list2 = list2.next
-            else:
-                tmp.append(list1.val)
-                list1 = list1.next
-        while list1:
-            tmp.append(list1.val)
-            list1 = list1.next
-        while list2:
-            tmp.append(list2.val)
-            list2 = list2.next
+        # if not (list1 or list2):
+        #     return None
+        # tmp = list()
+        # while list1 and list2:
+        #     if not list1 or list1.val > list2.val:
+        #         tmp.append(list2.val)
+        #         list2 = list2.next
+        #     else:
+        #         tmp.append(list1.val)
+        #         list1 = list1.next
+        # while list1:
+        #     tmp.append(list1.val)
+        #     list1 = list1.next
+        # while list2:
+        #     tmp.append(list2.val)
+        #     list2 = list2.next
+        #
+        # ans = ListNode(val=tmp[-1])
+        # for i in tmp[:-1][::-1]:
+        #     ans = ListNode(val=i, next=ans)
+        # return ans
+        dummy = ListNode()
+        tmp = dummy
 
-        ans = ListNode(val=tmp[-1])
-        for i in tmp[:-1][::-1]:
-            ans = ListNode(val=i, next=ans)
-        return ans
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tmp.next = list1
+                list1 = list1.next
+            else:
+                tmp.next = list2
+                list2 = list2.next
+            tmp = tmp.next
+
+        tmp.next = list1 or list2
+        return dummy.next
