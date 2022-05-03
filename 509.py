22cf3738a6becead00f2f8cb969a6ed47efe5e64
Jimmy Yeh <chienfeng0719@gmail.com>
2022-05-03 12:06:43 +0800

    Finish 509

---
+++ 509.py
@@ -0,0 +1,9 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [0, 1]
+        if n <= 1:
+            return dp[n]
+
+        for i in range(2, n + 1):
+            dp.append(dp[i - 1] + dp[i - 2])
+        return dp[-1]
