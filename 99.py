da678fe1b20eaa00719ebcd0fa5816d689197d54
Jimmy Yeh <chienfeng0719@gmail.com>
2022-06-01 17:58:52 +0800

    Update 99

--- 99.py
+++ 99.py
@@ -12,41 +12,37 @@ class Solution:
     2. 先將數字全部讀取出來之後排序, 接著update回每一個非None的節點
     """
 
-    def recoverTree(self, root: Optional[TreeNode]) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
-        # results = list()
-        # stack = list()
-        # current = root
-        # previous = 2 ** 31
-        # while current or stack:
-        #     while current:
-        #         stack.append(current)
-        #         current = current.left
-        #     current = stack.pop()
-        #     if previous and current.val < previous:
-        #         for result in results:
-        #             if current.val < result.val:
-        #                 result.val, current.val = current.val, result.val
-        #     results.append(current)
-        #     previous = current.val
-        #     current = current.right
-        # return root
-        values = list()
-        walk_stack = list()
-        node_stack = list()
-        current = root
+    # def recoverTree(self, root: Optional[TreeNode]) -> None:
+    #     results = list()
+    #     stack = list()
+    #     current = root
+    #     previous = 2 ** 31
+    #     while current or stack:
+    #         while current:
+    #             stack.append(current)
+    #             current = current.left
+    #         current = stack.pop()
+    #         if previous and current.val < previous:
+    #             for result in results:
+    #                 if current.val < result.val:
+    #                     result.val, current.val = current.val, result.val
+    #         results.append(current)
+    #         previous = current.val
+    #         current = current.right
+    #     return root
 
-        while current or walk_stack:
-            while current:
-                walk_stack.append(current)
-                current = current.left
-            current = walk_stack.pop()
-            values.append(current.val)
-            node_stack.append(current)
-            current = current.right
+    def dfs(self, root, nodes):
+        if not root:
+            return
+        self.dfs(root=root.left, nodes=nodes)
+        nodes.append(root)
+        self.dfs(root=root.right, nodes=nodes)
+
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        nodes = list()
+        self.dfs(root=root, nodes=nodes)
+        values = sorted([node.val for node in nodes])
 
         for index, value in enumerate(sorted(values)):
-            node_stack[index].val = value
+            nodes[index].val = value
         return root
