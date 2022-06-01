1a6713288ce4d674cf1d6ee525891c8ebed3745b
Jimmy Yeh <chienfeng0719@gmail.com>
2022-06-01 12:39:34 +0800

    Update 98

--- 98.py
+++ 98.py
@@ -6,7 +6,9 @@
 #         self.right = right
 class Solution:
     """
-    利用二元樹中序搜尋之結果為排序特性
+    兩種解題思路:
+    1. 利用二元樹中序搜尋之結果為排序特性
+    2. 定義出每個節點的範圍(floor, ceiling), 去做比較
     """
 
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
@@ -26,3 +28,14 @@ class Solution:
             if num >= result[index + 1]:
                 return False
         return True
+
+    # def bfs(self, root, floor, ceiling):
+    #     if not root:
+    #         return True
+    #     if not floor < root.val < ceiling:
+    #         return False
+    #     return self.bfs(root=root.left, floor=floor, ceiling=root.val) and \
+    #            self.bfs(root=root.right, floor=root.val, ceiling=ceiling)
+    #
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    #     return self.bfs(root=root, floor=-2 ** 31 - 1, ceiling=2 ** 31)
