45ff72dfd3dd5f9f97d85926be719ec5c0c34b19
Jimmy Yeh <chienfeng0719@gmail.com>
2022-09-10 15:28:40 +0800

    Update 102

--- 102.py
+++ 102.py
@@ -5,25 +5,42 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return root
+        # if not root:
+        #     return root
+        #
+        # level_dict = {}
+        # stack = [{'level': 0, 'node': root}]
+        #
+        # while stack:
+        #     root = stack.pop()
+        #     node, level = root['node'], root['level']
+        #
+        #     level_dict.setdefault(level, list())
+        #
+        #     level_dict[level].append(node.val)
+        #
+        #     if node.right:
+        #         stack.append({'level': level + 1, 'node': node.right})
+        #     if node.left:
+        #         stack.append({'level': level + 1, 'node': node.left})
+        #
+        # return list(level_dict.values())
 
-        level_dict = {}
-        stack = [{'level': 0, 'node': root}]
+        results = list()
+        queue = list()
+        queue.append(root)
 
-        while stack:
-            root = stack.pop()
-            node, level = root['node'], root['level']
+        while queue:
+            level = []
+            level_len = len(queue)
+            for _ in range(level_len):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                results.append(level)
 
-            level_dict.setdefault(level, list())
-
-            level_dict[level].append(node.val)
-
-            if node.right:
-                stack.append({'level': level + 1, 'node': node.right})
-            if node.left:
-                stack.append({'level': level + 1, 'node': node.left})
-
-        return list(level_dict.values())
+        return results
