ac0140ff4dd639ff580dd76b301933d5ced628e7
Jimmy Yeh <chienfeng0719@gmail.com>
2022-09-10 15:29:02 +0800

    Finish 107

---
+++ 107.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        results = list()
+        queue = list()
+        queue.append(root)
+
+        while queue:
+            level = []
+            level_len = len(queue)
+            for _ in range(level_len):
+                node = queue.pop(0)
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                results = [level] + results
+
+        return results
